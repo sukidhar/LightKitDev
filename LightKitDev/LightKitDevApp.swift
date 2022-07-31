f39594ea7bd08bfdbeb7ef38618d806865e34d96
sukidhar <sukidhar@gmail.com>
2022-07-31 18:18:09 +0530

    Initial Commit

---
+++ LightKitDev/LightKitDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightKitDevApp.swift
+//  LightKitDev
+//
+//  Created by sukidhar on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LightKitDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
